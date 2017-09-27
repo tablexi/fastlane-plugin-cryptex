ef800f9273390a43353581e09af936f15fc521c7
edwardlafoy <edwardlafoy@gmail.com>
2017-09-26 21:05:34 -0500

    Update cryptex_generate_keystore.rb

--- lib/fastlane/plugin/cryptex/actions/cryptex_generate_keystore.rb
+++ lib/fastlane/plugin/cryptex/actions/cryptex_generate_keystore.rb
@@ -3,7 +3,7 @@ module Fastlane
     class CryptexGenerateKeystoreAction < Action
       def self.run(params)
         require "fileutils"
-        cmd = "keytool -genkey -v -keystore #{File.expand_path(params[:destination])} -storepass #{params[:password]} -keypass #{params[:password]} -alias #{params[:alias]} -dname 'CN=#{params[:fullname]},L=#{params[:city]}' -validity 1000"
+        cmd = "keytool -genkey -v -keystore #{File.expand_path(params[:destination])} -storepass #{params[:keystore_password]} -keypass #{params[:key_password]} -alias #{params[:alias]} -dname 'CN=#{params[:fullname]},L=#{params[:city]}' -validity 1000"
         FastlaneCore::CommandExecutor.execute(command: cmd,
                                             print_all: true,
                                         print_command: true)
@@ -21,31 +21,31 @@ module Fastlane
 
       def self.available_options
         [
-          FastlaneCore::ConfigItem.new(key: :password,
-                                       env_name: "MATCH_KEYSTORE_PASSWORD",
+          FastlaneCore::ConfigItem.new(key: :keystore_password,
+                                       description: "Password for the Keystore",
+                                       optional: true,
+                                       is_string: true,
+                                       default_value: ""),
+          FastlaneCore::ConfigItem.new(key: :key_password,
                                        description: "Password for the Keystore",
                                        optional: true,
                                        is_string: true,
                                        default_value: ""),
           FastlaneCore::ConfigItem.new(key: :alias,
-                                       env_name: "MATCH_KEYSTORE_ALIAS",
                                        description: "ALIAS for the Keystore",
                                        optional: true,
                                        is_string: true,
                                        default_value: ""),
           FastlaneCore::ConfigItem.new(key: :destination,
-                                       env_name: "MATCH_KEYSTORE_DESTINATION",
                                        description: "Where to put decrypted keystore",
                                        optional: true,
                                        default_value: ""),
           FastlaneCore::ConfigItem.new(key: :fullname,
-                                       env_name: "MATCH_KEYSTORE_FULLNAME",
                                        description: "Fullname of keystore owner",
                                        optional: true,
                                        is_string: true,
                                        default_value: ""),
           FastlaneCore::ConfigItem.new(key: :city,
-                                       env_name: "MATCH_KEYSTORE_CITY",
                                        description: "City of keystore owner",
                                        optional: true,
                                        is_string: true,
